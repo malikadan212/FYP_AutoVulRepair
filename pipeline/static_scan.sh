ad3dfb13f06998cf8ce483931988859acb6e5905
malikadan21 <i221000@nu.edu.pk>
2025-09-14 17:11:39 +0500

    Initial commit: Static Analysis Setup with Cppcheck and Codeql

--- pipeline/static_scan.sh
+++ pipeline/static_scan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 ROOT_DIR="$SCRIPT_DIR/.."
 
 TARGET_PATH="${1:-$ROOT_DIR/src}"  # default: scan src/ if no path given
@@ -10,16 +10,34 @@ OUT_DIR="$ROOT_DIR/artifacts"
 mkdir -p "$OUT_DIR"
 
 TS=$(date +%Y%m%d-%H%M%S)
-OUTFILE="$OUT_DIR/cppcheck-report-$TS.xml"
+CPPCHECK_REPORT="${OUT_DIR}/cppcheck-report-${TS}.xml"
+CODEQL_REPORT="${OUT_DIR}/codeql-report-${TS}.sarif"
+CODEQL_DB="${OUT_DIR}/codeql_db-${TS}"
 
 if ! command -v cppcheck >/dev/null 2>&1; then
     echo "[!] cppcheck not installed. Run: sudo apt install cppcheck -y"
     exit 2
 fi
 
-echo "[*] Running Cppcheck on $TARGET_PATH ..."
-cppcheck --enable=all --inconclusive --quiet --xml --xml-version=2 "$TARGET_PATH" 2> "$OUTFILE" || true
+echo "[*] Running Cppcheck on ${TARGET_PATH} ..."
+cppcheck --enable=all --inconclusive --quiet --xml --xml-version=2 "${TARGET_PATH}" 2> "${CPPCHECK_REPORT}"
+echo "[*] Scan complete. Report written to ${CPPCHECK_REPORT}"
+echo "[*] To quickly preview issues: grep 'error' \"${CPPCHECK_REPORT}\" | head"
 
-echo "[*] Scan complete. Report written to $OUTFILE"
-echo "[*] To quickly preview issues: grep '<error' \"$OUTFILE\" | head"
+
+if ! command -v codeql > /dev/null 2>&1; then
+	echo "[!] Codeql not installed."
+	exit 2
+fi
+
+echo "[*] Creating CodeQL database for ${TARGET_PATH} ..."
+codeql database create "${CODEQL_DB}" --language=cpp --command="g++ -c test.cpp" --source-root="${TARGET_PATH}"
+
+echo "[*] Running CodeQL analysis for ${TARGET_PATH} ..."
+codeql database analyze "${CODEQL_DB}" --search-path="${ROOT_DIR}/tools/codeql-repo" --format=sarif-latest --output="${CODEQL_REPORT}" ${ROOT_DIR}/tools/codeql-repo/cpp/ql/src/codeql-suites/cpp-security-and-quality.qls
+
+echo "[*] CodeQL scan complete. Report written to ${CODEQL_REPORT}"
+
+#Post-processing
+python3 "${SCRIPT_DIR}/parse_reports.py" "${CPPCHECK_REPORT}" "${CODEQL_REPORT}"
 
